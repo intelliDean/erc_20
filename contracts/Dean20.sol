1d3fe88e711ba7f5aee7e85337c32e0a140a9ab3
intelliDean <intelliDean@gmail.com>
2024-02-16 16:07:15 +0100

    updated the code

--- contracts/Dean20.sol
+++ contracts/Dean20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IDean20} from "contracts/IDean20.sol";
+import "./IDean20.sol";
 
 contract Dean20 is IDean20 {
 
@@ -10,10 +10,11 @@ contract Dean20 is IDean20 {
     uint256 private s_totalSupply;
     address private s_owner;
     uint8 private s_tokenDecimal;
-    uint256 private constant MAX_SUPPLY = 30000000000000000000000000;
+    uint256 private immutable MAX_SUPPLY;
 
     mapping(address account => uint256 balance) private balances;
     mapping(address account => mapping(address spender => uint256)) private allowances;
+
     event Transfer(address from, address to, uint256 value);
 
     constructor() {
@@ -21,7 +22,7 @@ contract Dean20 is IDean20 {
         s_tokenSymbol = "DTK";
         s_tokenDecimal = 18;
         s_owner = tx.origin;
-        // mint(s_owner, 1000000000000000000000000);
+        MAX_SUPPLY = 30000000000000000000000000;
 
         uint256 initialSupply = 1000000000000000000000000;
         require(initialSupply <= MAX_SUPPLY, "Maximum Token Supply Reached");
@@ -29,83 +30,79 @@ contract Dean20 is IDean20 {
         balances[msg.sender] = balances[msg.sender] + initialSupply;
     }
 
-    function onlyOwner() private view {
+    function onlyOwner() private view { //done
         require(msg.sender == s_owner);
     }
 
-    function getTokenName() external view returns(string memory) {
-        return s_tokenName;
-    }
-    function getTokenSymbol() external view returns(string memory) {
-        return s_tokenSymbol;
-    }
-    function getTokenDecimal() external view returns(uint8) {
+    function getTokenDecimal() external view returns (uint8) {   //done
         return s_tokenDecimal;
     }
 
-    function getTotalSupply() external view returns(uint256) {
-        return s_totalSupply;
-    }
-
-    function name() external  view returns (string memory) {
+    function name() external view returns (string memory) {    //done
         return s_tokenName;
     }
 
-    function symbol() external view returns (string memory) {
+    function symbol() external view returns (string memory) {   //done
         return s_tokenSymbol;
     }
 
-    function decimals() external view returns (uint8) {
+    function decimals() external view returns (uint8) {     //done
         return s_tokenDecimal;
     }
 
-    function totalSupply() external view returns (uint256) {
-        return s_totalSupply;
+    function totalSupply() external view returns (uint256) {        //done
+        return revDecimals(s_totalSupply);
     }
 
-    function balanceOf(address _user) external view returns (uint256 balance) {
-        return balances[_user];
+    function balanceOf(address _user) external view returns (uint256 balance) {     //done
+        return revDecimals(balances[_user]);
     }
 
     function transfer(address _to, uint256 _value) external returns (bool success) {
         require(_to != address(0), "You cannot sender fund to Zero address");
         require(msg.sender != address(0), "Zero address cannot call this function");
-        require(balances[msg.sender] >= _value, "Insufficient balance");
 
-        balances[msg.sender] = balances[msg.sender] - _value;
-        balances[_to] = balances[_to] + _value;
+        uint256 deduction = doDecimals(_value) + (doDecimals(_value) * (100 / 10));
+
+        //burn 10% of the amount sent as charges
+        balances[msg.sender] = balances[msg.sender] - deduction;
+        s_totalSupply = s_totalSupply - (doDecimals(_value) * (100 / 10));
+
+        balances[_to] = balances[_to] + doDecimals(_value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
+        require(_to != address(0), "You cannot sender fund to Zero address");
+        require(msg.sender != address(0), "Zero address cannot call this function");
+        require(allowances[_from][_to] >= doDecimals(_value), "Insufficient Allowance Balance");
 
-        /*
-        i will check first if the _to is given approved to spend
-        if the _to is approved to spend, then transfer _value to _to
-        if _to is not approved to spend, check if msg.sender is approved to spend
-        if yes, then transfer to _to
-        */
-        require(allowances[_from][_to] >= _value, "Insufficient Balance");
-        allowances[_from][_to] = allowances[_from][_to] - _value;
-        balances[_to] = _value;
+
+        uint256 deduction = doDecimals(_value) + (doDecimals(_value) * (100 / 10));
+
+        balances[_from] = balances[_from] - deduction;
+        allowances[_from][_to] = allowances[_from][_to] - doDecimals(_value);
+
+        s_totalSupply = s_totalSupply - (doDecimals(_value) * (100 / 10));
+
+        balances[_to] = balances[_to] + doDecimals(_value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) external returns (bool success) {
-        //check that the owner has more than the value he's approving
-        //other sanity checks
+        require(msg.sender != address(0), "Zero address cannot call this method");
+        require(_spender != address(0), "Zero address cannot be spender");
+        require(balances[msg.sender] >= doDecimals(_value));
         allowances[msg.sender][_spender] = doDecimals(_value);
         return true;
     }
 
     function allowance(address _owner, address _spender) external view returns (uint256 remaining) {
-        return allowances[_owner][_spender];
+        return revDecimals(allowances[_owner][_spender]);
     }
 
-    function mint(address _account, uint256 _amount) public   {
-        //check that only user can call this function
-        //after all the checks
-       onlyOwner();
+    function mint(address _account, uint256 _amount) public {
+        onlyOwner();
         uint256 tempSupply = s_totalSupply + doDecimals(_amount);
         require(tempSupply <= MAX_SUPPLY, "Maximum Token Supply Reached");
 
@@ -114,15 +111,19 @@ contract Dean20 is IDean20 {
     }
 
     function burn(address _account, uint96 _amount) external {
-        //after all the checks
 
-           balances[_account] = balances[_account] - _amount;
-           s_totalSupply = s_totalSupply - _amount;
+        require(msg.sender != address(0), "Zero address cannot call this method");
+        balances[_account] = balances[_account] - doDecimals(_amount);
+        s_totalSupply = s_totalSupply - doDecimals(_amount);
 
-           balances[address(0)] = balances[address(0)] + _amount;
+        balances[address(0)] = balances[address(0)] + doDecimals(_amount);
     }
 
     function doDecimals(uint _amount) private pure returns (uint256) {
         return _amount * (10 ** 18);
+    }
+
+    function revDecimals(uint _amount) private pure returns (uint256) {
+        return _amount / (10 ** 18);
     }
 }
